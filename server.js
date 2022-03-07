d60b2cb7579ce4e850f659fa065d1c5e0e6bd11c
Chamara Abesinghe <akachamara@gmail.com>
2022-03-07 19:39:07 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const net = require('net');
+
+const server = net.createServer(socket => {
+    socket.on('data',data=>{
+        console.log(data.toString());
+        socket.write('HTTP/1.1 69 Api thamai hondatama kale\r\n');
+        socket.end((err)=>{console.log(err)});
+    });
+});
+
+server.listen(80);
